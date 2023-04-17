c16dcb4febdc1d43fa9a70720ac080e039fd1f6a
Adrian Fossli <adrianf@uia.no>
2023-04-17 18:29:36 +0200

    la til en seachbar tenker å legge den over drinklista til Math

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,5 +1,28 @@
 import DrinkList from "./DrinkList";
 import {StyleSheet, View} from "react-native";
+import SearchBar from "./SearchBar";
+import React, { useState } from 'react';
+
+
+const App = () => {
+    const [searchResults, setSearchResults] = useState([]);
+
+    const handleSearch = (searchTerm) => {
+        // perform search logic here, and update searchResults state
+        setSearchResults(/* search results based on searchTerm */);
+    };
+
+    return (
+        <View>
+            <SearchBar onSearch={handleSearch} />
+            <Text>{searchResults.length} results found</Text>
+            {/* display search results here */}
+        </View>
+    );
+};
+
+export default App;
+
 
 const HomeScreen = ({navigation}) => {
     return (
