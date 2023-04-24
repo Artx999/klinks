a1853b9c8abd030ca0a47ab41b098a808ceb6189
Artx999 <54852665+Artx999@users.noreply.github.com>
2023-04-24 19:19:30 +0200

    Set up dev build
    This is needed for Migrating from auth proxy and Expo Go to Development builds.
    https://github.com/expo/fyi/blob/main/auth-proxy-migration.md

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import "expo-dev-client";
 import {StatusBar} from 'expo-status-bar';
 import {NavigationContainer} from "@react-navigation/native";
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
