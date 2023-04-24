c652862ab05d9f837ad123e1aa3ed3c2ee7e72b4
Artx999 <54852665+Artx999@users.noreply.github.com>
2023-04-24 15:57:15 +0200

    Configured Authentication with Google
    Still need some work though. I also suspect this might not work in prod, but we'll see.

--- components/LoginScreen.js
+++ components/LoginScreen.js
@@ -1,9 +1,54 @@
-import {Button, StyleSheet, View} from "react-native";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
+
+WebBrowser.maybeCompleteAuthSession();
 
 const LoginScreen = ({navigation}) => {
+    const [token, setToken] = useState("");
+    const [userInfo, setUserInfo] = useState(null);
+
+    const [request, response, promptAsync] = Google.useAuthRequest({
+        clientId: "488317155424-og3ekdvksu4snuirldrbd5oo0ns2spkn.apps.googleusercontent.com"
+    });
+
+    useEffect(() => {
+        if (response?.type === "success") {
+            setToken(response.authentication.accessToken);
+            getUserInfo();
+        }
+    }, [response, token]);
+
+    const getUserInfo = async () => {
+        try {
+            const response = await fetch(
+                "https://www.googleapis.com/userinfo/v2/me",
+                {
+                    headers: { Authorization: `Bearer ${token}` },
+                }
+            );
+
+            const user = await response.json();
+            setUserInfo(user);
+        } catch (error) {
+            // Add your own error handler here
+        }
+    };
+
     return (
         <View style={styles.container}>
-            <Button title="Login with google"></Button>
+            {userInfo === null ? (
+                <Button
+                    title="Sign in with Google"
+                    disabled={!request}
+                    onPress={() => {
+                        promptAsync();
+                    }}
+                />
+            ) : (
+                <Text style={styles.text}>{userInfo.name}</Text>
+            )}
             <Button title="Home screen" onPress={() => navigation.navigate('Home')}></Button>
         </View>
     )
