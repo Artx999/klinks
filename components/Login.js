393117da6d665454b6361f5dfa669f0d7becd099
Artx999 <54852665+Artx999@users.noreply.github.com>
2023-05-06 03:37:16 +0200

    Removed unused imports from Login

--- components/Login.js
+++ components/Login.js
@@ -1,9 +1,8 @@
-import {useEffect, useState} from "react";
-import {StyleSheet, Text, View, Button} from "react-native";
+import {useEffect} from "react";
+import {StyleSheet, Text} from "react-native";
 import * as WebBrowser from "expo-web-browser";
 import * as Google from "expo-auth-session/providers/google";
 import {FontAwesome5} from '@expo/vector-icons';
-import * as AuthSession from "expo-auth-session";
 
 
 WebBrowser.maybeCompleteAuthSession();
